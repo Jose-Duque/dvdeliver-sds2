eda5bf91fd253cddef30afceebda6ff08822ddae
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-01-11 17:54:45 -0300

    Project created

---
+++ backend/src/test/java/com/devjc/dvdeliver/DvdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjc.dvdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
