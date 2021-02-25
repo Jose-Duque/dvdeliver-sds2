7a0d3aa82bb54cd64343e89b50230521e3fa38fa
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-25 10:25:42 -0300

    Add routes and StepsHearder

---
+++ front-web/src/Order/index.tsx
@@ -0,0 +1,16 @@
+import Navbar from '../Navbar';
+import  './styles.css';
+import {} from 'react-router-dom';
+import StepsHeader from './StepsHeader';
+
+const Order = () => {
+  return (
+    <>
+      <div className="orders-container">
+        <StepsHeader/>
+      </div>
+    </>
+  );
+}
+
+export default Order;
